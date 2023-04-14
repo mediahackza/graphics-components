d2ea2484163a9a82e178f265a9118c376858baee
Alastair Otter <alastair@mediahack.co.za>
2023-04-14 21:20:01 +0200

    Initial commit

---
+++ src/lib/Button.svelte
@@ -0,0 +1,16 @@
+<script>
+    console.log('Button');
+    
+</script>
+
+<div class="button">
+    Button
+</div>
+
+<style>
+    .button {
+        background: #eee;
+        padding: 1em;
+        border: 1px solid #ccc;
+    }
+</style>
